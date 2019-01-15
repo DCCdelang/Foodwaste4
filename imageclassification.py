a1c3158a12a9396befd832ccc6eeefb1f86645fb
DCCdelang <dccdelang@gmail.com>
2019-01-15 15:43:45 +0100

    definition grayscale

--- imageclassification.py
+++ imageclassification.py
@@ -11,11 +11,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Setting RGB to Grayscale via OpenCV library
-image = cv2.imread('C:/Users/djdcc_000/Documents/School/UNI/Beta-Gamma/2018-2019/Leren_en_Beslissen/Git/Foodwaste4/20190106_dataset_zero_foodwaste_uva/20181201155604_2e04dad4-f5a1-4400-93bb-9ce940ff7a23.jpg')
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-
-# cv2.imshow("Original image", image)
-# cv2.imshow("Grayscale image", gray)
-#
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+def RGB2GRAY(Imagepath):
+    image = cv2.imread(Imagepath)
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("Original image", image)
+    # cv2.imshow("Grayscale image", gray)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+return gray
