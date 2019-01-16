6467c3f56afbaf9d3044a639895514267e710c20
driffa <niekijzerman@kpnmail.nl>
2019-01-17 00:04:41 +0100

    functies toegevoegd

--- preprocessing.py
+++ preprocessing.py
@@ -3,31 +3,50 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import os
 
 # Setting RGB to Grayscale via OpenCV library
-def RGB2GRAY(Imagepath):
-    image = cv2.imread(Imagepath)
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("Original image", image)
-    # cv2.imshow("Grayscale image", gray)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    return gray
+# def RGB2GRAY(Imagepath):
+#     image = cv2.imread(Imagepath)
+#     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+#     return gray
 
 def image_to_matrix(picture):
 
     imgage = cv2.imread(picture, 0)
     img_reverted = cv2.bitwise_not(imgage)
     matrix_image = np.round((img_reverted / 255), 2)
-
     return matrix_image
 
 # Setting up csv to dictionary
-def csv_to_dict(csvfile):
-    reader = csv.DictReader(open(csvfile, 'r'))
-    dictionary = []
-    for image in reader:
-        dictionary.append(image)
-    return dictionary
+def csv_to_array(csvfile):
+    data = open(csvfile, 'rt')
+    reader = csv.reader(data)
+    data_array = []
 
-print(csv_to_dict("labels.csv"))
+    for row in reader:
+        semi_data = []
+        for i in range(len(row)):
+            semi_data.append(row[i])
+        data_array.append(semi_data)
+        data = data_array[1:]
+
+    return data
+
+#maakt een array met voor elke foto een lijst met: naam foto, vier getallen en matrix van elke foto
+# directory is path dat gaat naar map waar de foto's in staan, csvfile is "labels.csv"
+def find_pictures(directory, csvfile):
+    data = csv_to_array(csvfile)
+    i = 0
+
+    for filename in sorted(os.listdir(directory)):
+        if filename.endswith(".jpg"): 
+            matrix = image_to_matrix(filename)
+            data[i].append(matrix)
+            print(i)
+            print(data[i])
+            i += 1
+    return np.asarray(data)
+
+final_data = find_pictures("/Users/NiekIJzerman/Desktop/Beta-Gamma/Jaar-3/Blok-3/Leren en beslissen/git/Foodwaste4/", "labels.csv")
+
