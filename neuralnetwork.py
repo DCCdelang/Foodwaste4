b6ca71310e2ec131906b66eaeff65523e32e177a
Jeroenvanwely <jeroenikovanwely@gmail.com>
2019-01-21 12:39:41 +0100

    neural netwerk ebgin

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,1 +1,12 @@
-import leren.py
+import leren
+import preprocessing
+import val_train_split
+
+training_set, validation_set = val_train_split.split_csv("labels.csv", 0.2)
+for row in training_set:
+    image_matrix = preprocessing.image_to_matrix(row[0])
+    
+    print(leren.n_layer_output(image_matrix, leren.n_layer_init(1920,1)))
+
+x_train, y_train_raw = x_y_split(training_set)
+y_train = transform_y(y_train_raw)
